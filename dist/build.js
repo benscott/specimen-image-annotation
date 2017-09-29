2ed33bb8c826ae0304ed7bc973c42439a0b224ad
benscott <ben@benscott.co.uk>
2017-09-29 15:08:33 +0100

    Update build

--- dist/build.js
+++ dist/build.js
@@ -10490,7 +10490,7 @@ module.exports = "data:application/vnd.ms-fontobject;base64,n04AAEFNAAACAAIABAAA
 /***/ (function(module, exports) {
 
 module.exports = {
-  api: 'http://ckan-data-dev.nhm.ac.uk/api/specimen/transcription'
+  api: 'http://ckan-data-dev.nhm.ac.uk:5000/api/specimen/transcription'
 };
 
 /***/ }),
